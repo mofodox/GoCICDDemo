3742b02702aa58b48f2efbfc78b916bb95fa6c8c
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-06-11 15:15:35 +0800

    Update calc_goblin_test.go

--- calc_goblin_test.go
+++ calc_goblin_test.go
@@ -11,7 +11,7 @@ func TestCalc(t *testing.T) {
 	gob := Goblin(t)
 	gob.Describe("Calc File", func() {
 		gob.It("should add two numbers ", func() {
-			gob.Assert(Add(1, 2)).Equal(4)
+			gob.Assert(Add(1, 2)).Equal(3)
 			gob.Assert(Add(1, 0)).Equal(1)
 			gob.Assert(Add(2, -2)).Equal(0)
 		})
